f787d57d1a1551ce6a472a8655f218ac3751684f
Michael Völkel <eisflamme@mihahome.de>
2023-10-08 18:36:55 +0200

    removed some adjustments, still works

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -eu
 
-sed -i '/nameserver 185.12.64.1/d' /run/systemd/resolve/resolv.conf
-sed -i '/2a01:4ff:ff00::add:1/d' /run/systemd/resolve/resolv.conf
-sed -i '/search ./d' /run/systemd/resolve/resolv.conf
-
-cp /run/systemd/resolve/resolv.conf /etc/resolv-static.conf
-
 # apparently swap needs to be disabled for best stability
 swapoff -a
 # action is d, so it deletes lines that contain the word "swap"
